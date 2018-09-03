904e2e7b50164440feb86d55f248f0678c4a31eb
DinisOliveira <42917242+DinisOliveira@users.noreply.github.com>
2018-09-03 02:16:31 +0100

    Update app.py

--- app.py
+++ app.py
@@ -43,6 +43,26 @@ def verify_fb_token(token_sent):
         return request.args.get("hub.challenge")
     return 'Invalid verification token'
 
+# start:example-hello.py
+# Sentences we'll respond with if the user greeted us
+GREETING_KEYWORDS = ("hello", "hi", "greetings", "sup", "what's up",)
+
+GREETING_RESPONSES = ["'sup bro", "hey", "*nods*", "hey you get my snap?"]
+
+def check_for_greeting(sentence):
+    """If any of the words in the user's input was a greeting, return a greeting response"""
+    for word in sentence.words:
+        if word.lower() in GREETING_KEYWORDS:
+            return random.choice(GREETING_RESPONSES)
+# start:example-none.py
+# Sentences we'll respond with if we have no idea what the user just said
+NONE_RESPONSES = [
+    "uh whatever",
+    "meet me at the foosball table, bro?",
+    "code hard bro",
+    "want to bro down and crush code?",
+    "I'd like to add you to my professional network on LinkedIn",
+    "Have you closed your seed round, dog?",]
 
 #chooses a random message to send to the user
 def get_message():
