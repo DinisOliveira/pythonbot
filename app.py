a6f88ce6d709540718259daae58a3e62a9ca551b
DinisOliveira <42917242+DinisOliveira@users.noreply.github.com>
2018-09-03 02:18:52 +0100

    Update app.py

--- app.py
+++ app.py
@@ -64,12 +64,6 @@ NONE_RESPONSES = [
     "I'd like to add you to my professional network on LinkedIn",
     "Have you closed your seed round, dog?",]
 
-#chooses a random message to send to the user
-def get_message():
-    sample_responses = ["You are stunning!", "We're proud of you.", "Keep on being you!", "We're greatful to know you :)"]
-    # return selected item to the user
-    return random.choice(sample_responses)
-
 #uses PyMessenger to send response to user
 def send_message(recipient_id, response):
     #sends user the text message provided via input response parameter
