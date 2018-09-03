4db11306a1727d2418da03db214a490379c33eb6
DinisOliveira <42917242+DinisOliveira@users.noreply.github.com>
2018-09-03 02:31:14 +0100

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def receive_message():
                     send_message(recipient_id, response_sent_text)
                 #if user sends us a GIF, photo,video, or any other non-text item
                 if message['message'].get('attachments'):
-                    response_sent_nontext = get_message()
+                    response_sent_nontext = check_for_greeting(sentence)
                     send_message(recipient_id, response_sent_nontext)
     return "Message Processed"
 
